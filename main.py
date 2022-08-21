1f71256d60f3d24cc65947436bb5a4e87cb11599
Term-inator <1121745627@qq.com>
2022-08-21 23:25:38 +0800

    feat: parse parameters

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 @Date : 2022/8/20
 """
 from typing import List, Dict
+from rich.console import Console
 
 import command
 
@@ -12,6 +13,7 @@ command_list: List[command.Command] = [command.LoginCmd(), command.LogoutCmd(), 
 
 
 class CLI:
+    console = Console()
     command_map: Dict[str, command.Command] = {}
 
     def __init__(self):
@@ -20,14 +22,28 @@ class CLI:
             for alias in cmd.alias:
                 self.command_map[alias] = cmd
 
+    def _parseParams(self, params: List[str]) -> Dict[str, str | bool]:
+        res = {}
+        n = len(params)
+        for i in range(n):
+            if params[i][0] == '-':
+                param_name = params[i][1:]
+                print(param_name)
+                res[param_name] = True
+                if i + 1 < n and params[i + 1][0] != '-':
+                    res[param_name] = params[i + 1]
+                    i += 2
+        return res
+
     def run(self):
         while True:
-            cmd_str = input('>>> ')
+            self.console.print('>>> ', style='blue', end='')
+            cmd_str = input()
             tmp = cmd_str.strip().split()
             cmd_name = tmp[0]
             params = None
             if len(tmp) > 1:
-                params = tmp[1:]
+                params = self._parseParams(tmp[1:])
             print(cmd_name, params)
             if cmd_name in self.command_map:
                 cmd = self.command_map[cmd_name]
